04be7dd03b8e86fb38a8f6ae219a01e894802340
sfjj@gmail.com <akfhi@gmail.com>
2022-08-22 19:00:28 +0500

    project

---
+++ script.js
@@ -0,0 +1,8 @@
+const data = ['Иван' , 'Иванов' , 'Отдел разработки']
+
+function func(emp) {
+    let [name, surname, depsrtment] = emp
+    console.log(name, surname, depsrtment);
+}
+
+func (data); 
